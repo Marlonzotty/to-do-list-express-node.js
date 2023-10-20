5c9e87c4eb34d16898de0e51b0757fef0989b7aa
unknown <you@example.com>
2023-10-20 14:23:44 -0300

    commite de segurança

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const checkListRouter = require('./src/routes/checklist.js'); // Usar barras normais
+require('./config/database.js');
+
+
+const app = express();
+app.use(express.json());
+
+
+
+app.use('/checklists',checkListRouter);
+
+app.listen(3000, () => {
+    console.log('Servidor foi iniciado');
+});
